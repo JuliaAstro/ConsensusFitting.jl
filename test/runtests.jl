082ec8454d6e27f56b61345a30ed37788567c58c
cgarling <chris.t.garling@gmail.com>
2026-04-02 09:55:05 -0400

    [skip ci] Run doctests in test/runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,11 @@ using ConsensusFitting
 using Random
 using Test
 
+# Run doctests first
+using Documenter: DocMeta, doctest
+DocMeta.setdocmeta!(ConsensusFitting, :DocTestSetup, :(using ConsensusFitting); recursive=true)
+doctest(ConsensusFitting)
+
 @testset "ConsensusFitting.jl" begin
 
     include("RANSAC_tests.jl")
